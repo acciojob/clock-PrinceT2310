9d903569b82b6e5a96a198f7db79bb789568856b
PrinceT2310 <princethakur64365@gmail.com>
2025-09-30 15:02:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+        setInterval(() => {
+
+            let date = new Date();
+
+            let time = `${date.getHours()}: ${date.getMinutes()}: ${date.getSeconds()}`
+
+            let currDate = `${date.getDate()} / ${date.getMonth()} / ${date.getFullYear()}`
+            document.getElementById("timer").innerText = `${currDate}, ${time}`
+        }, 1000)
